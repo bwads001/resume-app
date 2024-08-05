4878bebb25c55b0d7582bbb0a67400c39aefa810
Bryan Wadsworth <wadsworth.bryan@gmail.com>
2024-08-05 09:50:37 -0700

    move button

--- components/delete-experience.tsx
+++ components/delete-experience.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { Button } from "@nextui-org/button";
 import { useFormState } from "react-dom";
 
 import { deleteExperience } from "@/app/lib/actions";
@@ -8,16 +7,16 @@ export function DeleteExperience({ id }: { id: number }) {
   const [error, action, isPending] = useFormState(deleteExperience, null);
 
   return (
-    <form action={action}>
-      <Button
-        className="text-xs text-secondary"
+    <form action={action} className="float-right justify-end">
+      <button
+        className="m-2 p-0 text-s text-neutral-300 bg-transparent hover:text-red-500"
         disabled={isPending}
         name="id"
         type="submit"
         value={id}
       >
-        Delete
-      </Button>
+        -
+      </button>
       {error && <p className="text-red-500">{error}</p>}
     </form>
   );
