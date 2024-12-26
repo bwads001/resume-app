82d164250a95a55ba400f91fcfb2489bc03d133a
Bryan Wadsworth <wadsworth.bryan@gmail.com>
2024-12-26 10:21:33 -0700

    whoops

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button"
-import { ArrowRight, Linkedin } from 'lucide-react'
+import { /* ArrowRight, */ Linkedin } from 'lucide-react'
 import Link from 'next/link'
 
 export function Hero() {
